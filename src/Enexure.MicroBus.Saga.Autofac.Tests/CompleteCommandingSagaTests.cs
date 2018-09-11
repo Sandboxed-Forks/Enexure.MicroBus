525053da66d3d23519be1a1aa3bd675261b5408d
dagilleland <dagilleland@users.noreply.github.com>
2018-09-11 01:09:10 -0600

    Unit test uncovers issue Lavinski/Enexure.MicroBus#29

---
+++ src/Enexure.MicroBus.Saga.Autofac.Tests/CompleteCommandingSagaTests.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Enexure.MicroBus.Autofac;
+using Enexure.MicroBus.Sagas;
+using Xunit;
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using System.Threading;
+
+namespace Enexure.MicroBus.Saga.Autofac.Tests
+{
+    public class CompleteCommandingSagaTests
+    {
+        private readonly Guid id = Guid.NewGuid();
+
+        [Fact]
+        public async Task RunningACommandingSagaToCompletion()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<TestCommandingSagaRepository>().AsImplementedInterfaces().SingleInstance();
+
+            var busBuilder = new BusBuilder()
+                .RegisterSaga<TestCommandingSaga>()
+                .RegisterHandlers(this.GetType().Assembly);
+
+            var container = builder
+                .RegisterMicroBus(busBuilder)
+                .Build();
+
+            var bus = container.Resolve<IMicroBus>();
+
+            await bus.PublishAsync(new SagaStartingAEvent() { CorrelationId = id });
+            string expected = "Started, Finished";
+            Thread.Sleep(5000);
+            Assert.Equal(expected, TestCommandingSaga.Status);
+        }
+    }
+}
---
+++ src/Enexure.MicroBus.Saga.Autofac.Tests/TestCommandingSaga.cs
@@ -0,0 +1,107 @@
+﻿using Enexure.MicroBus.Sagas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enexure.MicroBus.Saga.Autofac.Tests
+{
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+    public class TestCommandingSaga : ISaga,
+        ISagaStartedBy<SagaStartingAEvent>,
+        IEventHandler<SagaEndingEvent>
+    {
+        public Guid Id { get; protected set; }
+        public bool IsCompleted { get; protected set; }
+        public static string Status { get; protected set; }
+
+        private readonly IMicroBus Bus;
+        public TestCommandingSaga(IMicroBus bus)
+        {
+            Bus = bus;
+        }
+
+        public async Task Handle(SagaStartingAEvent @event)
+        {
+            Id = @event.CorrelationId;
+            Status = "Started, ";
+            await Bus.SendAsync(new EndSaga { CorrelationId = Id });
+        }
+
+        public async Task Handle(SagaEndingEvent @event)
+        {
+            Status += "Finished.";
+            IsCompleted = true;
+        }
+    }
+
+    public class EndSagaCommandHandler : ICommandHandler<EndSaga>
+    {
+        private readonly IMicroBus Bus;
+        public EndSagaCommandHandler(IMicroBus bus)
+        {
+            Bus = bus;
+        }
+
+        public Task Handle(EndSaga command)
+        {
+            Bus.PublishAsync(new SagaEndingEvent { CorrelationId = command.CorrelationId });
+            return Task.CompletedTask;
+        }
+    }
+
+    public class TestCommandingSagaRepository : ISagaRepository<TestCommandingSaga>
+    {
+        Dictionary<Guid, TestCommandingSaga> sagas = new Dictionary<Guid, TestCommandingSaga>();
+        private readonly IMicroBus Bus;
+        public TestCommandingSagaRepository(IMicroBus bus)
+        {
+            Bus = bus;
+        }
+
+        public Task CompleteAsync(TestCommandingSaga saga)
+        {
+            sagas.Remove(saga.Id);
+            return Task.CompletedTask;
+        }
+
+        public Task CreateAsync(TestCommandingSaga saga)
+        {
+            sagas.Add(saga.Id, saga);
+            return Task.CompletedTask;
+        }
+
+        public Task<TestCommandingSaga> FindAsync(IEvent message)
+        {
+            var correlatedMessage = message as IHaveCorrelationId;
+            if (correlatedMessage != null)
+            {
+                return FindById(correlatedMessage.CorrelationId);
+            }
+
+            throw new Exception("message must inherit from the interface IHaveCorrelationId");
+        }
+
+        public Task<TestCommandingSaga> FindById(Guid id)
+        {
+            return Task.FromResult(sagas.ContainsKey(id) ? sagas[id] : null);
+        }
+
+        public TestCommandingSaga NewSaga()
+        {
+            return new TestCommandingSaga(Bus);
+        }
+
+        public Task UpdateAsync(TestCommandingSaga saga)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+
+    public class EndSaga : ICommand
+    {
+        public Guid CorrelationId { get; set; }
+    }
+}
